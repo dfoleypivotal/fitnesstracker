c607c3c19fa8571409db3989ca8a9e46d28665f6
Dennis Foley <dfoley@pivotal.io>
2018-07-28 21:37:58 -0600

    Added scripts for Session Data

---
+++ curlAddSession.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Get the refreshed access token from the https://developers.google.com/oauthplayground
+if [[ -z "${ACCESS_TOKEN}" ]]; then
+  echo "ACCESS_TOKEN has not been set or is invalid. Go to https://developers.google.com/oauthplayground to generate a new token"
+  exit
+fi
+curl \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $ACCESS_TOKEN" \
+  -X PUT -d @sessionData.txt https://www.googleapis.com/fitness/v1/users/me/sessions/SessionId0001
