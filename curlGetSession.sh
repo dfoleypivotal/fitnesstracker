a6d258bc2ae3f7f4577e0b5b2be4a67489b3e2fb
Patrick Davies <patrick.davies@MBP6340.local>
2018-07-29 07:43:38 -0600

    fixed

--- curlGetSession.sh
+++ curlGetSession.sh
@@ -3,10 +3,8 @@
 if [[ -z "${ACCESS_TOKEN}" ]]; then
   echo "ACCESS_TOKEN has not been set or is invalid. Go to https://developers.google.com/oauthplayground to generate a new token"
   exit
-ficurl \
+fi
+curl \
   -H "Content-Type: application/json;encoding=utf-8" \
   -H "Authorization: Bearer $ACCESS_TOKEN" \
   -X GET https://www.googleapis.com/fitness/v1/users/me/sessions
-
-
-
