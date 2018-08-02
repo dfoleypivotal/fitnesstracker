d7980e2b172353d936c6cc70a6ba58db882fefd0
Patrick Davies <daviespatrickc@gmail.com>
2018-08-01 23:01:46 -0700

    curl commands working for get and put of sessions

--- createSessions.py
+++ createSessions.py
@@ -1,16 +1,71 @@
 import pycurl
 from StringIO import StringIO
+import os
+import configparser
+import calendar
+from datetime import datetime
+from urllib import urlencode
+import json
+import cStringIO
+from io import BytesIO
 
-buffer = StringIO()
-c = pycurl.Curl()
-c.setopt(c.URL, 'http://pycurl.io/')
-#c.setopt(c.WRITEDATA, buffer)
 
-c.perform()
-c.close()
+def date_to_nano(ts):
+    """
+    Takes a datetime object and returns POSIX UTC in nanoseconds
+    """
+    return calendar.timegm(ts.utctimetuple()) * int(1e2)
 
-body = buffer.getvalue()
+if __name__ == '__main__':
 
-# Body is a string in some encoding.
-# In Python 2, we can print it without knowing what the encoding is.
-print(body)
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    inputfile = "{}/settings.ini".format(cwd)
+    settings = configparser.ConfigParser()
+    settings.read(inputfile)
+    access_token = settings.get('FIT', 'api.accesstoken')
+
+    header = ['Content-type: application/json',
+              'Authorization: Bearer {}'.format(access_token)]
+
+    tnum =  date_to_nano(datetime.now())
+    session_id = 'RandallId{}'.format(tnum)
+
+    file = open("sessionData2.json","w")
+
+    post_data = {
+        "id": "{}".format(session_id),
+        "name": "Randalls example workout {}".format(str(tnum)),
+        "description": "A very intense workout by Randall",
+        "startTimeMillis": 1532539980000,
+        "endTimeMillis":   1532547180000,
+        "application": {
+            "packageName": "com.google.android.apps.fitness",
+            "version": "web"
+        },
+        "activityType": 108}
+
+    file.write(json.dumps(post_data))
+
+    file.close()
+
+    buf = cStringIO.StringIO()
+
+    c = pycurl.Curl()
+    c.setopt(c.URL, 'https://www.googleapis.com/fitness/v1/users/me/sessions/{}'.format(session_id))
+    c.setopt(c.HTTPHEADER, header)
+
+    c.setopt(c.UPLOAD, 1)
+    file = open('sessionData2.json')
+    c.setopt(c.READDATA, file)
+
+    c.perform()
+    print "status code: %s" % c.getinfo(pycurl.HTTP_CODE)
+
+    c.close()
+
+    # File must be kept open while Curl object is using it
+    file.close()
+
+    body = buf.getvalue()
+
+    print(body)
--- getSessions.py
+++ getSessions.py
@@ -2,6 +2,8 @@ import pycurl
 from StringIO import StringIO
 import os
 import configparser
+import cStringIO
+
 
 cwd = os.path.dirname(os.path.realpath(__file__))
 inputfile = "{}/settings.ini".format(cwd)
@@ -12,33 +14,19 @@ access_token = settings.get('FIT', 'api.accesstoken')
 header = ['Content-type: application/json;encoding=utf-8',
           'Authorization: Bearer {}'.format(access_token)]
 
+buf = cStringIO.StringIO()
 
-buffer = StringIO()
 c = pycurl.Curl()
 c.setopt(c.URL, 'https://www.googleapis.com/fitness/v1/users/me/sessions')
 c.setopt(c.HTTPHEADER, header)
 
-#c.setopt(c.WRITEDATA, buffer)
+c.setopt(c.WRITEFUNCTION, buf.write)
 
 c.perform()
 c.close()
 
-body = buffer.getvalue()
+body = buf.getvalue()
 
 # Body is a string in some encoding.
 # In Python 2, we can print it without knowing what the encoding is.
 print(body)
-
-
-'''
-#!/bin/bash
-# Get the refreshed access token "Bearer" from the https://developers.google.com/oauthplayground
-if [[ -z "${ACCESS_TOKEN}" ]]; then
-  echo "ACCESS_TOKEN has not been set or is invalid. Go to https://developers.google.com/oauthplayground to generate a new token"
-  exit
-fi
-curl \
-  -H "Content-Type: application/json;encoding=utf-8" \
-  -H "Authorization: Bearer $ACCESS_TOKEN" \
-  -X GET https://www.googleapis.com/fitness/v1/users/me/sessions
-'''
