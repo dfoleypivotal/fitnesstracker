707a1df603b26dfda836c2a171028716e1523290
Patrick Davies <daviespatrickc@gmail.com>
2018-08-03 08:06:11 -0700

    getSessions now returns all data from all datasources

--- getSessions.py
+++ getSessions.py
@@ -8,6 +8,37 @@ import calendar
 from datetime import datetime, timedelta
 import pytz
 
+class FitDataset:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def get_dataset_points(self, datasourceId, datasetId):
+        ds_header = ['Content-type: application/json;encoding=utf-8',
+                     'Authorization: Bearer {}'.format(self.access_token)]
+
+        buf = cStringIO.StringIO()
+
+        c = pycurl.Curl()
+        c.setopt(c.URL, 'https://www.googleapis.com/fitness/v1/users/me/dataSources/{}/datasets/{}'.format(datasourceId, datasetId))
+        c.setopt(c.HTTPHEADER, ds_header)
+
+        c.setopt(c.WRITEFUNCTION, buf.write)
+
+        c.perform()
+        c.close()
+
+        body = buf.getvalue()
+
+        # Body is a string in some encoding.
+        # In Python 2, we can print it without knowing what the encoding is.
+        # print(body)
+        if body == 'Not Found':
+            return None
+
+        result = json.loads(body)
+        point = result['point']
+        return point
+
 
 class FitDatasources:
     def __init__(self, access_token):
@@ -15,7 +46,7 @@ class FitDatasources:
 
     def get_datasources(self):
         ds_header = ['Content-type: application/json;encoding=utf-8',
-                          'Authorization: Bearer {}'.format(self.access_token)]
+                     'Authorization: Bearer {}'.format(self.access_token)]
 
         buf = cStringIO.StringIO()
 
@@ -32,14 +63,13 @@ class FitDatasources:
 
         # Body is a string in some encoding.
         # In Python 2, we can print it without knowing what the encoding is.
-        print(body)
+        # print(body)
 
         result = json.loads(body)
         if 'dataSource' in result:
             return result['dataSource']
         else:
             return None
-
 
 class FitSessions:
 
@@ -109,7 +139,27 @@ if __name__ == '__main__':
             print("Session: (id: {}, Start: {} {}, End: {} {})".format(session['id'], startTimeStr,
                                                                       session['startTimeMillis'],
                                                                       endTimeStr, session['endTimeMillis']))
-            # print("TZ {}, none {}".format("{}".format(dt_aware.strftime('%Y-%m-%d %H:%M:%S')), "{}".format(dt.strftime('%Y-%m-%d %H:%M:%S'))))
+            fit_dataset = FitDataset(access_token)
+            for datasource in datasources:
+                points = fit_dataset.get_dataset_points(datasource['dataStreamId'], session['startTimeMillis'] + '000000-' + session['endTimeMillis'] + '000000')
+                if points is not None and len(points) > 0:
+                    print("\tDataStreamID: {}".format(datasource['dataStreamId']))
+
+                    for point in points:
+                        print("\t\tValue: {}, StartTime: {}, EndTime: {}".format(point['value'], point['startTimeNanos'], point['endTimeNanos']))
+
+                        '''
+                        value = points[0]
+                        value_dict = value[0]
+                        value_str = ""
+                        if 'intVal' in value_dict:
+                            value_str = str(value_dict['intVal'])
+                        else:
+                            if 'fpVal' in value_dict:
+                                value_str = str(value_dict['fpVal'])
+    
+                        print("\tStore Value = {}, for DataStreamID: {}".format(value_str, datasource['dataStreamId']))
+                        '''
 
 
 
